6952cc0b6d6934be89c13767c9152ce4fcda46e8
Eve0784 <everceymedina@gmail.com>
2025-10-27 11:35:07 +0100

    esercizi

--- script.js
+++ script.js
@@ -90,6 +90,17 @@
 //     }
 //     }
 
+// for (let i=1 ; i<=100; i++){
+//     let quadrato= Math.sqrt(i);
+//     if(Number.isInteger(quadrato){
+//         console.log(i+ "*");
+//     }
+//     else{
+//         console.log(i);
+        
+//     }
+//     }
+
 
 // 10) chiedi all'utente un numero (positivo) di partenza e poi logga tutti i numeri dal numero dato fino a 0
 
@@ -127,3 +138,171 @@
 // let eta =parseInt(prompt("Quanti anni hai?"));
 // const ore = eta * 365 * 24; // anni × giorni × ore
 // alert("Hai vissuto circa " + ore + " ore.");
+
+
+// 13) Write a loop that makes seven calls to console.log to output the following triangle:
+
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+// let line= '';
+// for (let i = 0; i <= 7; i++) {
+//     console.log(line);
+//     line+= '#';
+// }
+
+// 14) Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+// For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), 
+// print "Buzz" instead.
+// When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3
+// and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+// for (let i = 1; i <=100; i++){
+//     if (i % 3===0) {
+//         console.log(" Fizz");
+        
+//     }
+//     else if (i % 5===0){
+//         console.log(" Buzz");
+//     }
+//     else{
+//         console.log(i);
+        
+//     }
+// }
+
+// for (let i = 1; i <=100; i++){
+//     if (i % 3===0 && i % 5===0){
+//         console.log('FizzBuzz'); 
+//      } 
+//     else if(i % 3===0) {
+//         console.log(" Fizz");   
+//     }
+//     else if (i % 5===0){
+//         console.log(" Buzz");
+//     }
+//     else{
+//         console.log(i);   
+//     }
+// }
+
+// for (let i = 1; i <=100; i++){
+//     if (i % 3===0 && i % 5 !==0){
+//         console.log('Fizz'); 
+//      } 
+//     else if(i % 5===0 && i %3 !==0) {
+//         console.log(" Buzz");   
+//     }
+//     else if (i % 3===0 && i % 5===0){
+//         console.log(" FizzBuzz");
+//     }
+//     else{
+//         console.log(i);   
+//     }
+// }
+
+// for (let i = 1; i <=100; i++){
+//     let response= '';
+//     if(i%3===0){
+//         response+= 'Fizz';
+//     }
+//     if (i%5===0) {
+//          response+= 'Buzz';     
+//     }
+//     if (response==='') {
+//         console.log(i);
+//     }
+//     else{
+//         console.log(response);
+        
+//     }
+// }
+
+
+// 15) Chessboard
+
+// Write a program that creates a string that represents an 8×8 grid, 
+// using newline characters to separate lines. At each position of the grid there is either a 
+// space or a "#" character. The characters should form a chessboard.
+// Passing this string to console.log should show something like this:
+
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # # 
+//  # # # #
+// # # # #
+// When you have a program that generates this pattern, define a binding 
+// size = 8 and change the program so that it works for any size, 
+// outputting a grid of the given width and height.
+// a)
+// for (let i = 0; i < 8; i++) {
+//     if (i % 2===0) {
+//         console.log(' # # # #');   
+//     }
+//     else{
+//         console.log('# # # # ');
+        
+//     }
+// }
+// b)
+// let chessboard = '';
+// for (let i = 0; i < 8; i++) {
+//     if (i % 2===0) {
+//         chessboard +=  ' # # # #\n';
+//     }
+//     else{
+//         chessboard += '# # # # \n'; 
+//     }
+// }
+// console.log(chessboard);
+
+// let size = 18;
+// let chessboard='';
+
+// for (let line = 0; line < size; line++) {
+//     for (let column = 0; column < size; column++) {
+//         if (line % 2 === 0) {
+//             if (column % 2 === 0) {
+//                 chessboard += ' ';
+//             }
+//             else {
+//                 chessboard += '#';
+//             }
+//         }
+//         else {
+//             if (column % 2 === 0) {
+//                 chessboard += '#';
+//             }
+//             else {
+//                 chessboard += ' ';
+//             }
+//         }
+//     }
+//     chessboard+= '\n';
+// }
+// console.log(chessboard);
+
+
+// let size = 18;
+// let chessboard='';
+
+// for (let line = 0; line < size; line++) {
+//     for (let column = 0; column < size; column++) {
+//          if ((line+ column) % 2 === 0){
+//                 chessboard += ' ';
+//             }
+//             else{
+//                 chessboard += '#';
+//         }
+//     }
+//          chessboard+= '\n';
+// }
+// console.log(chessboard);
